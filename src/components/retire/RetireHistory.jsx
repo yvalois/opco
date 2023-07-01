910717c6277840471ddb773f5c13ed317370f459
yvalois <yeison.valopez@gmail.com>
2023-07-01 11:34:08 -0500

    Retiro arreglado

--- src/components/retire/RetireHistory.jsx
+++ src/components/retire/RetireHistory.jsx
@@ -13,37 +13,23 @@ export const RetireHistory = ({ data, accountAddress }) => {
         Retiros anteriores
       </div>
       {/*Pasar a true*/}
-      {!accountAddress &&
+      {accountAddress &&
         data.map(data => (
           <div className="flex flex-col md:flex-row w-full my-4 rounded-lg p-4 bg-white text-black shadow-md space-y-4 md:space-y-0 md:space-x-4 justify-between items-start md:items-center">
-            {/* <div className="xxs:w-full flex flex-col space-y-2 jusify-center items-center">                  
+            <div className="xxs:w-full flex flex-col space-y-2 jusify-center items-center">                  
             <span className='text-gray-600 font-semibold'>Opco ingresado:</span> 
-            <span className='text-yellow-300 font-bold'>{parseFloat((data.opcoAmount / 10 ** 8))}</span>
+            <span className='text-black font-bold'>{parseFloat((data.opcoAmount / 10 ** 8))}</span>
             </div>
 
             <div className="xxs:w-full flex flex-col space-y-2 jusify-center items-center">  
             <span className='text-gray-600 font-semibold'>Busd a retirar:</span> 
-            <span className='text-yellow-300 font-bold'>{parseFloat(ethers.utils.formatEther(data.busdAmount))}</span>
+            <span className='text-black font-bold'>{parseFloat(ethers.utils.formatEther(data.busdAmount))}</span>
             </div>
 
             <div className="xxs:w-full flex flex-col space-y-2 jusify-center items-center"> 
             <span className='text-gray-600 font-semibold'>Dia de retiro:</span> 
-            <span className='text-yellow-300 font-bold'>{timestampToDate(parseInt(data.unlockTime))}</span>
-            </div> */}
-
-
-             <div className="xxs:w-full flex flex-col space-y-2 jusify-center items-center">
-              <span className='text-gray-600 font-semibold'>Opco ingresado:</span>
-              <span className='text-yellow-300 font-bold'>1</span>
-            </div>
-            <div className="xxs:w-full flex flex-col space-y-2 jusify-center items-center">
-              <span className='text-gray-600 font-semibold'>Busd a retirar:</span>
-              <span className='text-yellow-300 font-bold'>1</span>
-            </div>
-            <div className="xxs:w-full flex flex-col space-y-2 jusify-center items-center">
-              <span className='text-gray-600 font-semibold'>Dia de retiro:</span>
-              <span className='text-yellow-300 font-bold'>Ayer</span>
-            </div>
+            <span className='text-black font-bold'>{timestampToDate(parseInt(data.unlockTime))}</span>
+            </div> 
 
           </div>
 
--- src/components/retire/RetireNew.jsx
+++ src/components/retire/RetireNew.jsx
@@ -33,43 +33,27 @@ export const RetireNew = ({ data, accountAddress }) => {
             <div className="text-center py-4 bg-white text-lg text-black font-bold">
                 Retiros Pendientes
             </div>
-      {/*Pasar a true*/}
-      {!accountAddress         && data.map(data => (
+            {/*Pasar a true*/}
+            {accountAddress && data.map(data => (
                 <div className="flex flex-col md:flex-row w-full my-4 rounded-lg p-4 bg-white text-black shadow-md space-y-4 md:space-y-0 md:space-x-4 justify-between items-start md:items-center">
+
+
                     <div className="xxs:w-full flex flex-col space-y-2 jusify-center items-center">
                         <span className='text-gray-600 font-semibold'>Opco ingresado:</span>
-                        <span className='text-yellow-300 font-bold'>1</span>
+                        <span className='text-black font-bold'>{parseFloat((data.opcoAmount / 10 ** 8))}</span>
                     </div>
                     <div className="xxs:w-full flex flex-col space-y-2 jusify-center items-center">
-                        <span className='text-gray-600 font-semibold'>Busd a retirar:</span>
-                        <span className='text-yellow-300 font-bold'>1</span>
+                        <span className='text-gray-600 font-semibold'>Busd a retirar: </span>
+                        <span className='text-black font-bold'>{parseFloat(ethers.utils.formatEther(data.busdAmount))}</span>
                     </div>
                     <div className="xxs:w-full flex flex-col space-y-2 jusify-center items-center">
-                        <span className='text-gray-600 font-semibold'>Dia de retiro:</span>
-                        <span className='text-yellow-300 font-bold'>Ayer</span>
+                        <span className='text-gray-600 font-semibold'>dia de retiro: </span>
+                        <span className='text-black font-bold'>{timestampToDate(parseInt(data.unlockTime))}</span>
                     </div>
                     <button
                         className="xxs:self-center w-auto h-auto px-4 py-2 bg-yellow-300 text-black rounded-lg shadow-lg font-semibold"
                         onClick={() => retire(data.id)}
                     >Retirar</button>
-
-                    {/*
-                     <div className="xxs:w-full flex flex-col space-y-2 jusify-center items-center">
-                        <span className='text-gray-600 font-semibold'>Opco ingresado:</span>
-                        <span className='text-yellow-300 font-bold'>{parseFloat((data.opcoAmount / 10 ** 8))}</span>
-                    </div>
-                     <div className="xxs:w-full flex flex-col space-y-2 jusify-center items-center">
-                       <span className='text-gray-600 font-semibold'>Busd a retirar: </span>
-                        <span className='text-yellow-300 font-bold'>{parseFloat(ethers.utils.formatEther(data.busdAmount))}</span>
-                    </div>
-                     <div className="xxs:w-full flex flex-col space-y-2 jusify-center items-center">
-                       <span className='text-gray-600 font-semibold'>dia de retiro: </span>
-                        <span className='text-yellow-300 font-bold'>{timestampToDate(parseInt(data.unlockTime))}</span>
-                    </div>
-                    <button
-                        className="xxs:self-center w-auto h-auto px-4 py-2 bg-yellow-300 text-black rounded-lg shadow-lg font-semibold"
-                        onClick={() => retire(data.id)}
-                    >Retirar</button>  */}
 
                 </div>
             ))}
