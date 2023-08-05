03488f6f0e2b5f5433960a08cab325e314fa1e14
yvalois <yeison.valopez@gmail.com>
2023-08-04 21:08:44 -0500

    connect

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -137,7 +137,7 @@ export default function SideBar({ setIsOpen }) {
   return (
 
     <div className="h-full w-full  md:w-64 bg-gray-900 md:p-4 flex flex-col  md:items-start  overflow-hidden">
-    <div className='w-full h-full '>
+    <div >
     {routes.map((route, index) => (
         <div key={index} className="w-full h-auto flex overflow-y-hidden">
           {
