de11f16bec656e3654927f9dc9ca045b47044e71
yvalois <yeison.valopez@gmail.com>
2023-08-07 14:14:44 -0500

    tienda

--- src/store/layout/StoreLayout.js
+++ src/store/layout/StoreLayout.js
@@ -24,7 +24,7 @@ export default function StoreLayout() {
     }, [dispatch]);
 
   return (
-    <div className='w-full h-full flex flex-col items-center overflow-hidden'>
+    <div className='w-full h-full flex flex-col items-center overflow-hidden mt-[60px]'>
         <>
         <Navbar click={() => setSideToggle(true)} />
         <Backdrop show={sideToggle} click={() => setSideToggle(false)} />
